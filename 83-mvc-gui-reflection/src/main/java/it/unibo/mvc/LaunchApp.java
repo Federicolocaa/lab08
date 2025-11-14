34bf4dc06985b30c75b7d8907d9e95ca398f1fdf
Federico Locatelli <federicolocatelli@Mac.station>
2025-11-14 17:50:57 +0100

    Esercizio 83 completato

--- 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -1,48 +1,73 @@
 package it.unibo.mvc;
 
-import java.lang.reflect.InvocationTargetException;
-
+import it.unibo.mvc.api.DrawNumber;
 import it.unibo.mvc.api.DrawNumberController;
 import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
-import it.unibo.mvc.view.DrawNumberStandardOutputView;
-import it.unibo.mvc.view.DrawNumberSwingView;
+import it.unibo.mvc.view.DrawNumberSwingView; // Necessario per 'instanceof'
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 
 /**
  * Application entry-point.
+ * Carica dinamicamente le viste usando la riflessione.
  */
 public final class LaunchApp {
 
-    private LaunchApp() { }
+    // Costanti per zittire Checkstyle (errore MagicNumber e Stringhe)
+    private static final String VIEW_PACKAGE = "it.unibo.mvc.view.DrawNumber";
+    private static final String SWING_VIEW_NAME = "SwingView";
+    private static final String OUTPUT_VIEW_NAME = "StandardOutputView";
+    private static final int COPIES = 3;
+
+    private LaunchApp() {
+        // Costruttore privato per classe 'utility' (per Checkstyle)
+    }
 
     /**
      * Runs the application.
      *
-     * @param args ignored
+     * @param args command line arguments, ignored.
      * @throws ClassNotFoundException if the fetches class does not exist
      * @throws NoSuchMethodException if the 0-ary constructor do not exist
      * @throws InvocationTargetException if the constructor throws exceptions
      * @throws InstantiationException if the constructor throws exceptions
      * @throws IllegalAccessException in case of reflection issues
-     * @throws IllegalArgumentException in case of reflection issues
      */
-    public static void main(final String... args) {
-        final var model = new DrawNumberImpl();
+    public static void main(final String... args)
+            throws
+            ClassNotFoundException,
+            NoSuchMethodException,
+            InvocationTargetException,
+            InstantiationException,
+            IllegalAccessException {
+
+        final DrawNumber model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
 
-        final DrawNumberView swingView1 = new DrawNumberSwingView();
-        final DrawNumberView swingView2 = new DrawNumberSwingView();
-        final DrawNumberView consoleView = new DrawNumberStandardOutputView();
+        final List<String> viewClassNames = List.of(OUTPUT_VIEW_NAME, SWING_VIEW_NAME);
 
-        app.addView(swingView1);
-        app.addView(swingView2);
-        app.addView(consoleView);
-
-        swingView1.setController(app);
-        swingView2.setController(app);
-
-        swingView1.start();
-        swingView2.start();
+        for (final String viewName : viewClassNames) {
+            final Class<?> clazz = Class.forName(VIEW_PACKAGE + viewName);
+            final Constructor<?> ctor = clazz.getConstructor();
+            for (int i = 0; i < COPIES; i++) {
+                final Object newViewObject = ctor.newInstance();
+                if (DrawNumberView.class.isAssignableFrom(newViewObject.getClass())) {
+                    final DrawNumberView view = (DrawNumberView) newViewObject;
+                    app.addView(view);
+                    if (newViewObject instanceof DrawNumberSwingView) {
+                        view.setController(app);
+                        view.start();
+                    }
+                } else {
+                    throw new IllegalStateException(
+                        newViewObject.getClass() + " is not a subclass of " + DrawNumberView.class
+                    );
+                }
+            }
+        }
     }
 }
